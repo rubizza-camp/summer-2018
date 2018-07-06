405c776b9f12475c18d37e18f32427b9d9019aaf
Lika Lex <lika.lex555@gmail.com>
2018-07-06 21:56:56 +0300

    rename variables.

--- 2199/1/koans/triangle.rb
+++ 2199/1/koans/triangle.rb
@@ -13,27 +13,27 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  raise TriangleError if invalid_dimensions?(a, b, c) || invalid_triangle?(a, b, c)
-  classify_valid_triangle(a, b, c)
+def triangle(first, second, third)
+  raise TriangleError if invalid_dimensions?(first, second, third) || invalid_triangle?(first, second, third)
+  classify_valid_triangle(first, second, third)
 end
 
-def classify_valid_triangle(a, b, c)
-  if a == b && a == c
+def classify_valid_triangle(first, second, third)
+  if first == second && first == third
     :equilateral
-  elsif a == b || b == c || a == c
+  elsif first == second || second == third || first == third
     :isosceles
   else
     :scalene
   end
 end
 
-def invalid_dimensions?(a, b, c)
-  a <= 0 || b <= 0 || c <= 0
+def invalid_dimensions?(first, second, third)
+  first <= 0 || second <= 0 || third <= 0
 end
 
-def invalid_triangle?(a, b, c)
-  a + b <= c || b + c <= a || a + c <= b
+def invalid_triangle?(first, second, third)
+  first + second <= third || second + third <= first || first + third <= second
 end
 
 # Error class used in part 2.  No need to change this code.
