27bb4e1e39e7f1c31e8b42436c435c881a68cf9b
Vadim Serebrennikov <vadimserebrennikov@MacBook-Pro-Vadim.local>
2018-07-07 12:44:57 +0300

    Fix style bugs(part2)

--- 2364/1/koans/triangle.rb
+++ 2364/1/koans/triangle.rb
@@ -12,20 +12,16 @@
 #   about_triangle_project.rb
 # and
 #   about_triangle_project_2.rb
-def triangle(a, b, c)
-    sides = [a,b,c].sort
-    raise TriangleError if sides.any? { |side| side <= 0 }
-    raise TriangleError unless (sides[0] + sides[1]) > sides[2]
-    sides.uniq!
-    if sides.count == 1
-        :equilateral
-        elsif sides.count == 2
-        :isosceles
-        else
-        :scalene
-    end
+def triangle(side_a, side_b, side_c)
+  sides = [side_a, side_b, side_c].sort
+  raise TriangleError if sides.any? { |side| side <= 0 }
+  raise TriangleError unless (sides[0] + sides[1]) > sides[2]
+  sides.uniq!
+  hash = Hash.new(:scalene)
+  hash[1] = :equilateral
+  hash[2] = :isosceles
+  hash[sides.count]
 end
-
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
