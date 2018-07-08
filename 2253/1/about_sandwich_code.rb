a803c2553d6a2364c6b625ea8eb4f1b447b671e3
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-09 00:32:33 +0300

    Update about_sandwich_code.rb

--- 2253/1/about_sandwich_code.rb
+++ 2253/1/about_sandwich_code.rb
@@ -14,22 +14,22 @@ class AboutSandwichCode < Neo::Koan
   end
 
   def test_counting_lines
-    assert_equal 4, count_lines("example_file.txt")
+    assert_equal 4, count_lines('example_file.txt')
   end
 
   # ------------------------------------------------------------------
 
   def find_line(file_name)
     file = open(file_name)
-    while line = file.gets
-      return line if line.match(/e/)
+    while (line = file.gets)
+      return line if line =~ /e/
     end
   ensure
     file.close if file
   end
 
   def test_finding_lines
-    assert_equal "test\n", find_line("example_file.txt")
+    assert_equal "test\n", find_line('example_file.txt')
   end
 
   # ------------------------------------------------------------------
@@ -89,7 +89,7 @@ class AboutSandwichCode < Neo::Koan
   end
 
   def test_finding_lines2
-    assert_equal "test\n", find_line2("example_file.txt")
+    assert_equal "test\n", find_line2('example_file.txt')
   end
 
   # ------------------------------------------------------------------
@@ -97,15 +97,12 @@ class AboutSandwichCode < Neo::Koan
   def count_lines3(file_name)
     open(file_name) do |file|
       count = 0
-      while file.gets
-        count += 1
-      end
+      count += 1 while file.gets
       count
     end
   end
 
   def test_open_handles_the_file_sandwich_when_given_a_block
-    assert_equal 4, count_lines3("example_file.txt")
+    assert_equal 4, count_lines3('example_file.txt')
   end
-
 end
