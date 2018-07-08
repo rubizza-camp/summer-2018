625b48185c84f4f0a1c1fdd8469e8663d4294616
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-08 23:59:01 +0300

    Update triangle.rb

--- 2253/1/triangle.rb
+++ 2253/1/triangle.rb
@@ -21,8 +21,8 @@ def triangle(a, b, c)
   elsif b == c
     :isosceles
   else
-  :scalene
-  end            	
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
