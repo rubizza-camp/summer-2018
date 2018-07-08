625deb0fdac0c071e33b37968dc917209bc18789
Polina Andrianova (loonary) <40717699+Apachina@users.noreply.github.com>
2018-07-09 01:16:49 +0300

    Update about_scoring_project.rb

--- 2420/1/koans/about_scoring_project.rb
+++ 2420/1/koans/about_scoring_project.rb
@@ -1,35 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 def score(dice)
-  total = 0
-  count = [0, 0, 0, 0, 0, 0]
+  result = 0
+  scores = Hash.new 0
 
-  dice.each do |die|
-    count[die - 1] += 1
+  dice.each do |n|
+    scores[n] += 1
   end
 
-  # iterate over each, and handle points for singles and triples
-  count.each_with_index do |count, index|
-    if count == 3
-      total = doTriples(index + 1, total)
-    elsif count < 3
-      total = doSingles(index + 1, count, total)
-    elsif count > 3
-      total = doTriples(index + 1, total)
-      total = doSingles(index + 1, count % 3, total)
-    end
+  scores.each do |key, value|
+    triplets, remainer = value / 3, value % 3
+
+    result += (100 * remainer) + (1000 * triplets) if key == 1
+    result += (50 * remainer) + (500 * triplets) if key == 5
+    result += (100 * key) if (key != 1 && key != 5 && value >= 3)
   end
 
-  total
-end
-
-def doTriples(number, total)
-  if number == 1
-    total += 1000
-  else
-    total += (number) * 100
-  end
-  total
+  result
 end
 
 def doSingles(number, count, total)
