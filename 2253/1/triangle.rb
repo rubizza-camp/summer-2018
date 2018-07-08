4f3bd831fc6b085c5c9fb10b607e64fd5b75a9a7
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-08 23:35:13 +0300

    Update triangle.rb

--- 2253/1/triangle.rb
+++ 2253/1/triangle.rb
@@ -15,23 +15,18 @@
 #
 def triangle(a, b, c)
   raise TriangleError if [a,b,c].min <= 0 or a + b <= c or a + c <= b or b + c <= a	
-  a == b ? a == c ? :equilateral : :isosceles : a == c ? :isosceles : b == c ? :isosceles : :scalene
   if a == b 
     if a == c
       :equilateral
     else
       :isosceles
     end
+  elsif a == c
+    :isosceles
+  elsif b == c
+    :isosceles
   else
-    if a == c
-      :isosceles
-    else
-      if b == c
-        :isosceles
-      else
-        :scalene
-      end
-    end
+  :scalene
   end            	
 end
 
