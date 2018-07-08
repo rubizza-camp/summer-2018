b1de2b9d3ad99f6eee1a0d2ef1d24e35dbad98bf
Anton <40821074+AntonNaumchik@users.noreply.github.com>
2018-07-08 22:23:56 +0300

    fix a spase around operators #18

--- 2370/1/triangle.rb
+++ 2370/1/triangle.rb
@@ -15,7 +15,7 @@
 #
 def triangle(a, b, c)
   raise TriangleError if a<=0 or b<=0 or c<=0
-  raise TriangleError if a+b<=c or b+c<=a or a+c<=b
+  raise TriangleError if a + b<=c or b + c<=a || a + c<=b
   return :equilateral if a==b and a==c                 
   return :isosceles if a==b or b==c or a==c
   :scalene                                                                                                       
