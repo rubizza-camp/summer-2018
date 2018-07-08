40de5dd051a857b835575e53c623facad58e0490
Anton <40821074+AntonNaumchik@users.noreply.github.com>
2018-07-08 22:26:07 +0300

    fix space around operators #17

--- 2370/1/triangle.rb
+++ 2370/1/triangle.rb
@@ -14,7 +14,7 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  raise TriangleError if a<=0 or b<=0 or c<=0
+  raise TriangleError if a <=0 or b <=0 || c <=0
   raise TriangleError if a + b<=c or b + c<=a || a + c<=b
   return :equilateral if a==b and a==c                 
   return :isosceles if a==b or b==c or a==c
