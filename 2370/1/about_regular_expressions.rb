3ce38c634c4c746c30b0b86985c6b9fe82115484
Anton <40821074+AntonNaumchik@users.noreply.github.com>
2018-07-08 23:35:08 +0300

    Fixed single-quoted strings in lines 159, 154, 113

--- 2370/1/about_regular_expressions.rb
+++ 2370/1/about_regular_expressions.rb
@@ -93,12 +93,12 @@ class AboutRegularExpressions < Neo::Koan
 
   def test_slash_a_anchors_to_the_start_of_the_string
     assert_equal 'start', "start end"[/\Astart/]
-    assert_equal nil, "start end"[/\Aend/]
+    assert_equal nil, 'start end'[/\Aend/]
   end
 
   def test_slash_z_anchors_to_the_end_of_the_string
-    assert_equal 'end', "start end"[/end\z/]
-    assert_equal nil, "start end"[/start\z/]
+    assert_equal 'end', 'start end'[/end\z/]
+    assert_equal nil, 'start end'[/start\z/]
   end
 
   def test_caret_anchors_to_the_start_of_lines
@@ -110,35 +110,35 @@ class AboutRegularExpressions < Neo::Koan
   end
 
   def test_slash_b_anchors_to_a_word_boundary
-    assert_equal 'vines', "bovine vines"[/\bvine./]
+    assert_equal 'vines', 'bovine vines'[/\bvine./]
   end
 
   # ------------------------------------------------------------------
 
   def test_parentheses_group_contents
-    assert_equal 'hahaha', "ahahaha"[/(ha)+/]
+    assert_equal 'hahaha', 'ahahaha'[/(ha)+/]
   end
 
   # ------------------------------------------------------------------
 
   def test_parentheses_also_capture_matched_content_by_number
-    assert_equal 'Gray', "Gray, James"[/(\w+), (\w+)/, 1]
-    assert_equal 'James', "Gray, James"[/(\w+), (\w+)/, 2]
+    assert_equal 'Gray', 'Gray, James'[/(\w+), (\w+)/, 1]
+    assert_equal 'James', 'Gray, James'[/(\w+), (\w+)/, 2]
   end
 
   def test_variables_can_also_be_used_to_access_captures
-    assert_equal 'Gray, James', "Name:  Gray, James"[/(\w+), (\w+)/]
-    assert_equal 'Gray', $1
-    assert_equal 'James', $2
+    assert_equal 'Gray, James', 'Name:  Gray, James'[/(\w+), (\w+)/]
+    assert_equal 'Gray', Regexp.last_match(1)
+    assert_equal 'James', Regexp.last_match(2)
   end
 
   # ------------------------------------------------------------------
 
   def test_a_vertical_pipe_means_or
     grays = /(James|Dana|Summer) Gray/
-    assert_equal 'James Gray', "James Gray"[grays]
-    assert_equal 'Summer', "Summer Gray"[grays, 1]
-    assert_equal nil, "Jim Gray"[grays, 1]
+    assert_equal 'James Gray', 'James Gray'[grays]
+    assert_equal 'Summer', 'Summer Gray'[grays, 1]
+    assert_equal nil, 'Jim Gray'[grays, 1]
   end
 
   # THINK ABOUT IT:
@@ -148,14 +148,14 @@ class AboutRegularExpressions < Neo::Koan
   # ------------------------------------------------------------------
 
   def test_scan_is_like_find_all
-    assert_equal ['one', 'two', 'three'], "one two-three".scan(/\w+/)
+    assert_equal %w[one two three], 'one two-three'.scan(/\w+/)
   end
 
   def test_sub_is_like_find_and_replace
-    assert_equal 'one t-three', "one two-three".sub(/(t\w*)/) { $1[0, 1] }
+    assert_equal 'one t-three', 'one two-three'.sub(/(t\w*)/) { $1[0, 1] }
   end
 
   def test_gsub_is_like_find_and_replace_all
-    assert_equal 'one t-t', "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
+    assert_equal 'one t-t', 'one two-three'.gsub(/(t\w*)/) { Regexp.last_match(1)[0, 1] }
   end
 end
