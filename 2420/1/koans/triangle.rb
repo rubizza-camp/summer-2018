e24723e4411afbf04dfa463184a9d0eda7fefe65
Polina Andrianova (loonary) <40717699+Apachina@users.noreply.github.com>
2018-07-09 22:12:41 +0300

    Update triangle.rb

--- 2420/1/koans/triangle.rb
+++ 2420/1/koans/triangle.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 # rubocop:disable Metrics/AbcSize
+# frozen_string_literal: true
 # rubocop:disable Metrics/LineLength
 # rubocop:disable Layout/Tab
 # rubocop:disable Metrics/MethodLength
