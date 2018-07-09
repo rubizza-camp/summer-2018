8af546fff472ef38b48b0221f9149702776a00e1
SadTigger <rammiel@yandex.ru>
2018-07-09 23:09:50 +0300

    About_true_and_false fixes and etc

--- 2217/1/koans/about_true_and_false.rb
+++ 2217/1/koans/about_true_and_false.rb
@@ -3,7 +3,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # class AboutTrueAndFalse
-# This class smells of :reek:TooManyStatements and :reek:ControlParameter
+# This class smells of :reek:ControlParameter
 class AboutTrueAndFalse < Neo::Koan
   def truth_value(condition)
     if condition
@@ -25,6 +25,7 @@ class AboutTrueAndFalse < Neo::Koan
     assert_equal :false_stuff, truth_value(nil)
   end
 
+  #This method smells of :reek:TooManyStatements 
   def test_everything_else_is_treated_as_true
     assert_equal :true_stuff, truth_value(1)
     assert_equal :true_stuff, truth_value(0)
