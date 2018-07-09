1872e91d35164ff36dbe49cbc8e3211eac785ce4
Aleksei <Lazarenko1294@yandex.ru>
2018-07-09 14:58:38 +0300

    Fix about_strings.rb and about_symbols.rb

--- 2355/1/koans/about_strings.rb
+++ 2355/1/koans/about_strings.rb
@@ -1,8 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutStrings < Neo::Koan
+  # rubocop:disable Style/WordArray
   def test_double_quoted_strings_are_strings
-    string = "Hello, World"
+    string = 'Hello, World'
     assert_equal true, string.is_a?(String)
   end
 
@@ -144,38 +145,38 @@ EOS
   end
 
   def test_you_can_get_a_single_character_from_a_string
-    string = "Bacon, lettuce and tomato"
-    assert_equal "a", string[1]
+    string = 'Bacon, lettuce and tomato'
+    assert_equal 'a', string[1]
 
     # Surprised?
   end
 
-  in_ruby_version("1.8") do
+  in_ruby_version('1.8') do
     def test_in_older_ruby_single_characters_are_represented_by_integers
-      assert_equal "a", ?a
+      assert_equal 'a', ?a
       assert_equal __, ?a == 97
 
       assert_equal __, ?b == (?a + 1)
     end
   end
 
-  in_ruby_version("1.9", "2") do
+  in_ruby_version('1.9', '2') do
     def test_in_modern_ruby_single_characters_are_represented_by_strings
-      assert_equal "a", ?a
+      assert_equal 'a', ?a
       assert_equal false, ?a == 97
     end
   end
 
   def test_strings_can_be_split
-    string = "Sausage Egg Cheese"
+    string = 'Sausage Egg Cheese'
     words = string.split
-    assert_equal ["Sausage", "Egg", "Cheese"], words
+    assert_equal ['Sausage', 'Egg', 'Cheese'], words
   end
 
   def test_strings_can_be_split_with_different_patterns
-    string = "the:rain:in:spain"
+    string = 'the:rain:in:spain'
     words = string.split(/:/)
-    assert_equal ["the", "rain", "in", "spain"], words
+    assert_equal ['the', 'rain', 'in', 'spain'], words
 
     # NOTE: Patterns are formed from Regular Expressions.  Ruby has a
     # very powerful Regular Expression library.  We will become
@@ -183,13 +184,13 @@ EOS
   end
 
   def test_strings_can_be_joined
-    words = ["Now", "is", "the", "time"]
-    assert_equal "Now is the time", words.join(" ")
+    words = ['Now', 'is', 'the', 'time']
+    assert_equal 'Now is the time', words.join(' ')
   end
 
   def test_strings_are_unique_objects
-    a = "a string"
-    b = "a string"
+    a = 'a string'
+    b = 'a string'
 
     assert_equal true, a           == b
     assert_equal false, a.object_id == b.object_id
--- 2355/1/koans/about_symbols.rb
+++ 2355/1/koans/about_symbols.rb
@@ -36,7 +36,7 @@ class AboutSymbols < Neo::Koan
   in_ruby_version('mri') do
     RubyConstant = "What is the sound of one hand clapping?"
     def test_constants_become_symbols
-      all_symbols_as_strings = Symbol.all_symbols.map { |x| x.to_s }
+      all_symbols_as_strings = Symbol.all_symbols.map { |xex| xex.to_s }
 
       assert_equal false, all_symbols_as_strings.include?(__)
     end
