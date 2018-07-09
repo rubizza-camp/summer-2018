e09d531afa4db530eb5680d719e774337691989e
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-10 01:42:18 +0300

    Update about_open_classes.rb

--- 2253/1/about_open_classes.rb
+++ 2253/1/about_open_classes.rb
@@ -17,23 +17,26 @@ class AboutOpenClasses < Neo::Koan
   # Open the existing Dog class and add a new method.
   class Dog
     def wag
-      "HAPPY"
+      'HAPPY'
     end
   end
 
   def test_after_reopening_dogs_can_both_wag_and_bark
     fido = Dog.new
-    assert_equal "HAPPY", fido.wag
-    assert_equal "WOOF", fido.bark
+    assert_equal 'HAPPY', fido.wag
+    assert_equal 'WOOF', fido.bark
   end
 
   # ------------------------------------------------------------------
-
+  
+  # rubocop:disable Style/ClassAndModuleChildren
+  # :nodoc:
   class ::Integer
     def even?
-      (self % 2) == 0
+      (self % 2).zero?
     end
   end
+  # rubocop:enable Style/ClassAndModuleChildren
 
   def test_even_existing_built_in_classes_can_be_reopened
     assert_equal false, 1.even?
