133ce16ea9e5a3df25204e9b919c1bcd9691febd
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-10 01:40:11 +0300

    Update about_modules.rb

--- 2253/1/about_modules.rb
+++ 2253/1/about_modules.rb
@@ -1,10 +1,13 @@
  require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+# :nodoc:
 class AboutModules < Neo::Koan
   module Nameable
+    # rubocop:disable Naming/AccessorMethodName
     def set_name(new_name)
       @name = new_name
     end
+    # rubocop:enable Naming/AccessorMethodName
 
     def here
       :in_module
@@ -19,17 +22,18 @@ class AboutModules < Neo::Koan
 
   # ------------------------------------------------------------------
 
+  # :nodoc:
   class Dog
     include Nameable
 
     attr_reader :name
 
     def initialize
-      @name = "Fido"
+      @name = 'Fido'
     end
 
     def bark
-      "WOOF"
+      'WOOF'
     end
 
     def here
@@ -39,21 +43,21 @@ class AboutModules < Neo::Koan
 
   def test_normal_methods_are_available_in_the_object
     fido = Dog.new
-    assert_equal "WOOF", fido.bark
+    assert_equal 'WOOF', fido.bark
   end
 
   def test_module_methods_are_also_available_in_the_object
     fido = Dog.new
     assert_nothing_raised do
-      fido.set_name("Rover")
+      fido.set_name('Rover')
     end
   end
 
   def test_module_methods_can_affect_instance_variables_in_the_object
     fido = Dog.new
-    assert_equal "Fido", fido.name
-    fido.set_name("Rover")
-    assert_equal "Rover", fido.name
+    assert_equal 'Fido', fido.name
+    fido.set_name('Rover')
+    assert_equal 'Rover', fido.name
   end
 
   def test_classes_can_override_module_methods
