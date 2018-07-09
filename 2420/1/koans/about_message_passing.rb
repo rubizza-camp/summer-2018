e648521fb60f6ee5323005f659c2fe12618cf3c7
Polina Andrianova (loonary) <40717699+Apachina@users.noreply.github.com>
2018-07-09 11:42:46 +0300

    Update about_message_passing.rb

--- 2420/1/koans/about_message_passing.rb
+++ 2420/1/koans/about_message_passing.rb
@@ -127,7 +127,7 @@ class AboutMessagePassing < Neo::Koan
 
     assert_equal 'Someone called foobar with <>', catcher.foobar
     assert_equal 'Someone called foobaz with <1>', catcher.foobaz(1)
-    assert_equal 'Someone called sum with <1, 2, 3, 4, 5, 6>', catcher.sum(1,2,3,4,5,6)
+    assert_equal 'Someone called sum with <1, 2, 3, 4, 5, 6>', catcher.sum(1, 2, 3, 4, 5, 6)
   end
 
   def test_catching_messages_makes_respond_to_lie
