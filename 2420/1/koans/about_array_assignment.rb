1f19fdafb19e2a58377575647471297ddd49e75d
Polina Andrianova (loonary) <40717699+Apachina@users.noreply.github.com>
2018-07-09 11:47:34 +0300

    Update about_array_assignment.rb

--- 2420/1/koans/about_array_assignment.rb
+++ 2420/1/koans/about_array_assignment.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+# class AboutArrayAssignment
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
     names = ['John', 'Smith']
