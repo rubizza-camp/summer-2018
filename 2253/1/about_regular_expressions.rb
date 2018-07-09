e1046483e5f109e75fee7bdd738bd61413457884
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-10 00:18:48 +0300

    Update about_regular_expressions.rb

--- 2253/1/about_regular_expressions.rb
+++ 2253/1/about_regular_expressions.rb
@@ -50,17 +50,17 @@ class AboutRegularExpressions < Neo::Koan
   # ------------------------------------------------------------------
 
   def test_character_classes_give_options_for_a_character
-    animals = ["cat", "bat", "rat", "zat"]
-    assert_equal ["cat", "bat", "rat"], animals.select { |a| a[/[cbr]at/] }
+    animals = %w[cat bat rat zat]
+    assert_equal (%w[cat bat rat], animals.select { |a| a[/[cbr]at/] })
   end
 
   def test_slash_d_is_a_shortcut_for_a_digit_character_class
-    assert_equal "42", "the number is 42"[/[0123456789]+/]
-    assert_equal "42", "the number is 42"[/\d+/]
+    assert_equal '42', 'the number is 42'[/[0123456789]+/]
+    assert_equal '42', 'the number is 42'[/\d+/]
   end
 
   def test_character_classes_can_include_ranges
-    assert_equal "42", "the number is 42"[/[0-9]+/]
+    assert_equal '42', 'the number is 42'[/[0-9]+/]
   end
 
   def test_slash_s_is_a_shortcut_for_a_whitespace_character_class
@@ -69,24 +69,24 @@ class AboutRegularExpressions < Neo::Koan
 
   def test_slash_w_is_a_shortcut_for_a_word_character_class
     # NOTE:  This is more like how a programmer might define a word.
-    assert_equal "variable_1", "variable_1 = 42"[/[a-zA-Z0-9_]+/]
-    assert_equal "variable_1", "variable_1 = 42"[/\w+/]
+    assert_equal 'variable_1', 'variable_1 = 42'[/[a-zA-Z0-9_]+/]
+    assert_equal 'variable_1', 'variable_1 = 42'[/\w+/]
   end
 
   def test_period_is_a_shortcut_for_any_non_newline_character
-    assert_equal "abc", "abc\n123"[/a.+/]
+    assert_equal 'abc', 'abc\n123'[/a.+/]
   end
 
   def test_a_character_class_can_be_negated
-    assert_equal "the number is ", "the number is 42"[/[^0-9]+/]
+    assert_equal 'the number is ', 'the number is 42'[/[^0-9]+/]
   end
 
   def test_shortcut_character_classes_are_negated_with_capitals
-    assert_equal "the number is ", "the number is 42"[/\D+/]
+    assert_equal 'the number is ', 'the number is 42'[/\D+/]
     assert_equal "space:", "space: \t\n"[/\S+/]
     # ... a programmer would most likely do
-    assert_equal " = ", "variable_1 = 42"[/[^a-zA-Z0-9_]+/]
-    assert_equal " = ", "variable_1 = 42"[/\W+/]
+    assert_equal ' = ', 'variable_1 = 42'[/[^a-zA-Z0-9_]+/]
+    assert_equal ' = ', 'variable_1 = 42'[/\W+/]
   end
 
   # ------------------------------------------------------------------
@@ -102,11 +102,11 @@ class AboutRegularExpressions < Neo::Koan
   end
 
   def test_caret_anchors_to_the_start_of_lines
-    assert_equal '2', 'num 42\n2 lines'[/^\d+/]
+    assert_equal '2', "num 42\n2 lines"[/^\d+/]
   end
 
   def test_dollar_sign_anchors_to_the_end_of_lines
-    assert_equal '42', '2 lines\nnum 42'[/\d+$/]
+    assert_equal '42', "2 lines\nnum 42"[/\d+$/]
   end
 
   def test_slash_b_anchors_to_a_word_boundary
@@ -148,14 +148,14 @@ class AboutRegularExpressions < Neo::Koan
   # ------------------------------------------------------------------
 
   def test_scan_is_like_find_all
-    assert_equal %w(one two three), "one two-three".scan(/\w+/)
+    assert_equal %w(one two three), 'one two-three'.scan(/\w+/)
   end
 
   def test_sub_is_like_find_and_replace
-    assert_equal "one t-three", "one two-three".sub(/(t\w*)/) { Regexp.last_match[1][0, 1] }
+    assert_equal 'one t-three', 'one two-three'.sub(/(t\w*)/) { Regexp.last_match[1][0, 1] }
   end
 
   def test_gsub_is_like_find_and_replace_all
-    assert_equal "one t-t", "one two-three".gsub(/(t\w*)/) { Regexp.last_match[1][0, 1] }
+    assert_equal 'one t-t', 'one two-three'.gsub(/(t\w*)/) { Regexp.last_match[1][0, 1] }
   end
 end
