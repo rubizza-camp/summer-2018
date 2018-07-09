be24b61d29f90f54dc0039b9ef6fb0dd80ef8c26
Aleksei <Lazarenko1294@yandex.ru>
2018-07-09 21:16:36 +0300

    Fix tabs in triangle.rb

--- 2355/1/koans/triangle.rb
+++ 2355/1/koans/triangle.rb
@@ -18,24 +18,24 @@
 # This method smells of :reek:TooManyStatements
 # This method smells of :reek:FeatureEnvy
 def triangle(first_side, second_side, third_side)
-	if [first_side, second_side,third_side].any? {|x| x <= 0}
-   	raise TriangleError, 'Sides must have positive length' 
-	end
+  if [first_side, second_side,third_side].any? {|x| x <= 0}
+  raise TriangleError, 'Sides must have positive length' 
+  end
 
-	sides = [first_side, second_side, third_side].sort
+  sides = [first_side, second_side, third_side].sort
 
-   unless sides[0] + sides[1] > sides[2]
-   	raise TriangleError, 'Does not satisfy triangle inequality'
-	end
+  unless sides[0] + sides[1] > sides[2]
+  raise TriangleError, 'Does not satisfy triangle inequality'
+  end
 
-	case 
-		when first_side == second_side && second_side == third_side
-			return :equilateral
-		when first_side == second_side || second_side == third_side || first_side == third_side
-			return :isosceles
-		else
-			return :scalene
-	end
+  case 
+    when first_side == second_side && second_side == third_side
+      return :equilateral
+    when first_side == second_side || second_side == third_side || first_side == third_side
+      return :isosceles
+    else
+      return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
