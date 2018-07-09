6ddb7def10029b6ddb1f3aca75baeb74d2998f08
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-10 01:47:35 +0300

    Update about_proxy_object_project.rb

--- 2253/1/about_proxy_object_project.rb
+++ 2253/1/about_proxy_object_project.rb
@@ -12,12 +12,14 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # missing handler and any other supporting methods.  The specification
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
+# rubocop:disable Style/MethodMissing
+# :nodoc:
 class Proxy
   def initialize(target_object)
     @object = target_object
-    @message_array=[]
+    @message_array = []
   end
-  
+
   def method_missing(method_name, *args, &block)
     @message_array << method_name
     @object.send(method_name, *args, &block)
@@ -36,6 +38,7 @@ class Proxy
   end
 end
 
+# rubocop:enable Style/MethodMissing
 # The proxy object should pass the following Koan:
 #
 class AboutProxyObjectProject < Neo::Koan
