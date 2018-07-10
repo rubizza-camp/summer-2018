380a47cce5977e3a219b01971f8df44195a2efab
SergeiHrenovsky <sergeihrenovsky@yandex.by>
2018-07-11 00:22:23 +0300

    home task 1 attempt 5

--- 2309/1/koans/about_message_passing.rb
+++ 2309/1/koans/about_message_passing.rb
@@ -144,6 +144,7 @@ class AboutMessagePassing < Neo::Koan
   # ------------------------------------------------------------------
 
   class WellBehavedFooCatcher
+    #rubocop:disable Style/MethodMissing
     def method_missing(method_name, *args, &block)
       if method_name.to_s[0, 3] == 'foo'
         'Foo to you too'
@@ -151,6 +152,7 @@ class AboutMessagePassing < Neo::Koan
         super
       end
     end
+    #rubocop:enable Style/MethodMissing
   end
 
   def test_foo_method_are_caught
