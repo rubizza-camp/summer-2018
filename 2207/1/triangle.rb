0abc1fc338b6807f35a04fc005c307e9ec5933aa
SvintBel <40833597+svintbel@users.noreply.github.com>
2018-07-10 20:17:38 +0300

    Change all triangle.rb to do test green

--- 2207/1/triangle.rb
+++ 2207/1/triangle.rb
@@ -1,5 +1,3 @@
-# :reek:TooManyStatements
-# :reek:FeatureEnvy
 # Triangle Project Code.
 
 # Triangle analyzes the lengths of the sides of a triangle
@@ -15,14 +13,21 @@
 # and
 #   about_triangle_project_2.rb
 #
+
 # This method smells of :reek:UtilityFunction
 # This method smells of :reek:FeatureEnvy
 # This method smells of :reek:UncommunicativeMethodName
 # This method smells of :reek:UncommunicativeVariableName
 # This method smells of :reek:TooManyStatements
 # This method smells of :reek:FeatureEnvy
-def triangle_error(side_a, side_b, side_c)
-  raise TriangleError if side_a + side_b <= side_c || side_b == side_a + side_c || [side_a, side_b, side_c].min < 0
+def select_type_triangle(per_a, per_b, per_c)
+  if per_a == per_b && per_b == per_c
+    :equilateral
+  elsif per_a == per_b || per_a == per_c || per_b == per_c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # This method smells of :reek:UtilityFunction
@@ -31,14 +36,14 @@ end
 # This method smells of :reek:UncommunicativeVariableName
 # This method smells of :reek:TooManyStatements
 # This method smells of :reek:FeatureEnvy
-def triangle(side_a, side_b, side_c)
-  if side_a == side_b && side_b == side_c
-    return :equilateral
-  elsif side_a == side_b || side_b == side_c || side_a == side_c
-    return :isosceles
-  else
-    return :scalene
-  end
+def triangle(per_a, per_b, per_c)
+  s = (per_a + per_b + per_c) / 2.0
+  # the following must be positive to be a valid triangle
+  ok = (s - per_a) * (s - per_b) * (s - per_c)
+
+  raise TriangleError if per_a <= 0 || per_b <= 0 || per_c <= 0 || ok <= 0
+
+  select_type_triangle(per_a, per_b, per_c)
 end
 
 # Error class used in part 2.  No need to change this code.
