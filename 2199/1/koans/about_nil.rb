8b1bb74102f80089f9df6a61f46289ab339da2d8
Любовь Онуфриюк <lika.lex555@gmail.com>
2018-07-10 09:40:54 +0300

    Update about_nil.rb

--- 2199/1/koans/about_nil.rb
+++ 2199/1/koans/about_nil.rb
@@ -5,6 +5,7 @@ class AboutNil < Neo::Koan
     assert_equal true, nil.is_a?(Object), 'Unlike NULL in other languages'
   end
 
+  # rubocop:disable Layout/CommentIndentation, Layout/IndentationWidth
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
     # What happens when you call a method that doesn't exist.  The
     # following begin/rescue/end code block captures the exception and
@@ -20,6 +21,7 @@ class AboutNil < Neo::Koan
       assert_match(/undefined method `some_method_nil_doesnt_know_about' for nil:NilClass/, ex.message)
     # rubocop:enable Layout/IndentationConsistency
   end
+  # rubocop:enable Layout/CommentIndentation, Layout/IndentationWidth
 
   # :reek:NilCheck
   def test_nil_has_a_few_methods_defined_on_it
