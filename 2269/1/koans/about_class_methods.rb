43280bbc6215d2a81205ccd1b5c9379a91dd6e3c
Evenstein <star78@tut.by>
2018-07-11 00:39:55 +0300

    another Task #1

--- 2269/1/koans/about_class_methods.rb
+++ 2269/1/koans/about_class_methods.rb
@@ -110,9 +110,11 @@ class AboutClassMethods < Neo::Koan
 
   # ------------------------------------------------------------------
   # Doggy class assss dddfrt fgghybfvcddddccdc
+  # rubocop:disable Style/Documentation
   LAST_EXPRESSION_IN_CLASS_STATEMENT = class Dog
                                          21
                                        end
+  # rubocop:enable Style/Documentation
 
   def test_class_statements_return_the_value_of_their_last_expression
     assert_equal 21, LAST_EXPRESSION_IN_CLASS_STATEMENT
@@ -120,9 +122,11 @@ class AboutClassMethods < Neo::Koan
 
   # ------------------------------------------------------------------
   # A Doggy class assss dddfrt fgghybfvcddddccdc
+  # rubocop:disable Style/Documentation
   SELF_INSIDE_OF_CLASS_STATEMENT = class Dog
                                      self
                                    end
+  # rubocop:enable Style/Documentation
 
   def test_self_while_inside_class_is_class_object_not_instance
     assert_equal true, Dog == SELF_INSIDE_OF_CLASS_STATEMENT
