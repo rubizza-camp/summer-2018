6454d5fe938330809eb925eb175a12fe1031119a
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-11 00:34:31 +0300

    Update about_message_passing.rb

--- 2253/1/about_message_passing.rb
+++ 2253/1/about_message_passing.rb
@@ -26,8 +26,8 @@ class AboutMessagePassing < Neo::Koan
     mc = MessageCatcher.new
 
     assert mc.send('caught?')
-    assert mc.send('caught' + '?')  # What do you need to add to the first string?
-    assert mc.send('CAUGHT?'.downcase)  # What would you need to do to the string?
+    assert mc.send('caught' + '?') # What do you need to add to the first string?
+    assert mc.send('CAUGHT?'.downcase) # What would you need to do to the string?
   end
 
   def test_send_with_underscores_will_also_send_messages
@@ -140,7 +140,7 @@ class AboutMessagePassing < Neo::Koan
   end
 
   # ------------------------------------------------------------------
-  
+
   # :nodoc:
   class WellBehavedFooCatcher
     def method_missing(method_name, *args, &block)
