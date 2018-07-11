42fb954656b3c2b09c7a75c0a5a0086f4fc21223
Ctalk3r <40827175+Ctalk3r@users.noreply.github.com>
2018-07-11 14:02:58 +0300

    Update about_message_passing.rb

--- 2253/1/about_message_passing.rb
+++ 2253/1/about_message_passing.rb
@@ -125,7 +125,7 @@ class AboutMessagePassing < Neo::Koan
   def test_all_messages_are_caught
     catcher = AllMessageCatcher.new
 
-    assert_equal 'Someone called foobar with <>"', catcher.foobar
+    assert_equal 'Someone called foobar with <>', catcher.foobar
     assert_equal 'Someone called foobaz with <1>', catcher.foobaz(1)
     assert_equal 'Someone called sum with <1, 2, 3, 4, 5, 6>', catcher.sum(1, 2, 3, 4, 5, 6)
   end
