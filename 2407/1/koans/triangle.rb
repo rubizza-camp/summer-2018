d39ca20ebefa2a091b4445825e3e6bd793c31614
Pharethi <aleksei_antihovich@mail.ru>
2018-07-11 14:59:09 +0300

    2407-1

--- 2407/1/koans/triangle.rb
+++ 2407/1/koans/triangle.rb
@@ -12,11 +12,11 @@
 #   about_triangle_project.rb
 # and
 #   about_triangle_project_2.rb
+
 # This method smells of :reek:UtilityFunction
 # This method smells of :reek:FeatureEnvy
 # This method smells of :reek:TooManyStatements
 def triangle(side_a, side_b, side_c)
-  # WRITE THIS CODE
 
   sides = [side_a, side_b, side_c].sort
   raise TriangleError if sides.any? { |side| side <= 0 }
@@ -26,19 +26,6 @@ def triangle(side_a, side_b, side_c)
   hash[1] = :equilateral
   hash[2] = :isosceles
   hash[sides.count]
-
-  # s = (side_a + side_b + side_c) / 2.0
-  # ok = (s - side_a) * (s - side_b) * (s - side_c)
-  # if side_a <= 0 || side_b <= 0 || side_c <= 0 || ok <= 0 then
-  #   raise TriangleError
-  # end
-  # if side_a == side_b && side_b == side_c then
-  #   :equilateral
-  # elsif side_a == side_b || side_a == side_c || side_b == side_c then
-  #   :isosceles
-  # else
-  #   :scalene
-  # end
 end
 
 # Error class used in part 2.  No need to change this code.
