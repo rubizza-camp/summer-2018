3c8c17aaabfd4f8bd799f6ce6f639f8bad657a3d
Tanya Akulich <akulich.tanya@gmail.com>
2018-07-11 14:49:57 +0300

    move it move it to separate folder and remove submodule

---
+++ 2211/1/koans/triangle.rb
@@ -0,0 +1,34 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+# :reek:UncommunicativeVariableName
+# :reek:UncommunicativeParameterName
+# rubocop: disable Naming/UncommunicativeMethodParamName
+# rubocop: disable Style/SignalException
+def triangle(a, b, c)
+  # WRITE THIS CODE
+  #--
+  a, b, c = [a, b, c].sort
+  fail TriangleError if (a + b) <= c
+  sides = [a, b, c].uniq
+  [nil, :equilateral, :isosceles, :scalene][sides.size]
+  #++
+end
+# rubocop: enable Style/SignalException
+# rubocop: enable Naming/UncommunicativeMethodParamName
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
