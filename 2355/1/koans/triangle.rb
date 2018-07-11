f5b27479266e343d02b143ec60893f04b8198a1b
Aleksei <Lazarenko1294@yandex.ru>
2018-07-11 21:47:24 +0300

    Fix methods in triangle.rb

--- 2355/1/koans/triangle.rb
+++ 2355/1/koans/triangle.rb
@@ -18,7 +18,7 @@
 # rubocop:disable Layout/CaseIndentation
 # This method smells of :reek:UtilityFunction
 # This method smells of :reek:DuplicateMethodCall
-def kind_of_triangle(first_side, second_side, third_side)
+def triangle_type(first_side, second_side, third_side)
   case
     when first_side == second_side && second_side == third_side
       :equilateral
@@ -31,18 +31,22 @@ end
 
 # This method smells of :reek:UncommunicativeVariableName
 # This method smells of :reek:UtilityFunction
-def triangle(first_side, second_side, third_side)
-  if [first_side, second_side, third_side].any? { |x| x <= 0 }
+def triangle(*args)
+  if args.size != 3
+    raise TriangleError, 'There are must be three sides'
+  end
+
+  if args.any? { |x| x <= 0 }
     raise TriangleError, 'Sides must have positive length'
   end
 
-  sorted_sides = [first_side, second_side, third_side].sort
+  sorted_sides = args.sort
 
   unless sorted_sides[0] + sorted_sides[1] > sorted_sides[2]
     raise TriangleError, 'Does not satisfy triangle inequality'
   end
 
-  kind_of_triangle(first_side, second_side, third_side)
+  triangle_type(first_side, second_side, third_side)
 end
 
 # Error class used in part 2.  No need to change this code.
