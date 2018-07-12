265340173a3633d9ed8460171f143170aa339c6e
Kondratuk Gleb <kondratukgleb76@gmail.com>
2018-07-13 00:34:14 +0300

    almost

--- 2386/1/koans/about_message_passing.rb
+++ 2386/1/koans/about_message_passing.rb
@@ -114,7 +114,7 @@ class AboutMessagePassing < Neo::Koan
 
   # :reek:ManualDispatch
   # :reek:UtilityFunction
-  # # rubocop:disable Style/MethodMissing
+  # rubocop:disable Style/MethodMissing
   class AllMessageCatcher
     def method_missing(method_name, *args)
       "Someone called #{method_name} with <#{args.join(', ')}>"
@@ -143,6 +143,7 @@ class AboutMessagePassing < Neo::Koan
 
   # :reek:ManualDispatch
   # :reek:UtilityFunction
+  # rubocop:disable Style/MethodMissing
   class WellBehavedFooCatcher
     def method_missing(method_name, *args, &block)
       if method_name.to_s[0, 3] == 'foo'
