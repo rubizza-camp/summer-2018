0c2e2c5935305e5163148942370032673995f8d6
Kondratuk Gleb <kondratukgleb76@gmail.com>
2018-07-16 00:00:21 +0300

    good homework-2

---
+++ 2386/2/Participant.rb
@@ -0,0 +1,13 @@
+# :reek:TooManyInstanceVariables
+# Class of Participant
+class Participant
+  attr_reader :name, :battles, :bad_words, :bad_in_round, :words_in_round
+
+  def initialize(object)
+    @name = object[:name]
+    @battles = object[:battles]
+    @bad_words = object[:bad_words]
+    @bad_in_round = object[:bad_in_round]
+    @words_in_round = object[:words_in_round]
+  end
+end
