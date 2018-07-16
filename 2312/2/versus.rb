d87ed2bc682ee0c90777c465d39c79a81e0f8c6b
lstpsche <lstpsche@gmail.com>
2018-07-16 12:22:50 +0300

    Fixed rubocop issues. Looking for reek issues.

--- 2312/2/versus.rb
+++ 2312/2/versus.rb
@@ -1,19 +1,19 @@
 require 'russian_obscenity'
 require 'terminal-table'
 
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Metrics/PerceivedComplexity
 # info[rapper_name] = [num_of_battles, num_of_rounds, aver_all_words_per_round, whole_num_of_bad_words,
-#                             aver_num_of_bad_words_per_battle, words_top_list{}]
-# :reek:TooManyStatements
-# :reek:NestedIterators
-# :reek:FeatureEnvy
+#                             aver_num_of_bad_words_per_battle, words_top_list{}
+
+def collect_battles
+  all_battles = Dir.entries('./').select do |entry|
+    !entry.start_with?('.') && File.exist?(entry) && File.extname(entry) == ''
+  end
+  all_battles
+end
+
 def run
+  all_battles = collect_battles
   ARGV.each do |command|
-    all_battles = Dir.entries('./').select do |entry|
-      !entry.start_with?('.') && File.exist?(entry) && File.extname(entry) == ''
-    end
     all_battles.sort!
     info = collect_names(all_battles)
     if command.include?('--top-bad-words=')
@@ -21,34 +21,42 @@ def run
     elsif command.include?('--top-words=')
       top_words(command.split('=')[1].to_i)
     end
-    # File.delete('rappers_name.lst')
   end
 end
 
-# :reek:TooManyStatements
-# :reek:FeatureEnvy
 def top_bad(num_top, info, all_battles)
   info.keys.each { |rapper| info = analyze(all_battles, rapper, info) }
   output_table = []
+  fill_table_global(info, output_table)
+  puts Terminal::Table.new rows: output_table[0...num_top]
+end
+
+def fill_table_global(info, output_table)
   counter = -1
   info.sort_by { |_key, value| value[3] }.reverse.to_h.keys.each do |rapper|
     counter += 1
-    output_table[counter] = [rapper]
-    output_table[counter] << "#{info[rapper][0]} battles" << "#{info[rapper][3]} bad words"
-    output_table[counter] << "#{info[rapper][4]} bad words per battle" << "#{info[rapper][2]} words per round"
+    output_table = fill_table_first(info, rapper, output_table, counter)
+    output_table = fill_table_second(info, rapper, output_table, counter)
   end
-  puts Terminal::Table.new rows: output_table[0...num_top]
+end
+
+def fill_table_first(info, rapper, output_table, counter)
+  output_table[counter] = [rapper]
+  output_table[counter] << "#{info[rapper][0]} battles" << "#{info[rapper][3]} bad words"
+  output_table
+end
+
+def fill_table_second(info, rapper, output_table, counter)
+  output_table[counter] << "#{info[rapper][4]} bad words per battle" << "#{info[rapper][2]} words per round"
+  output_table
 end
 
 def top_words() end
 
-# :reek:UtilityFunction
 def double?(battle)
   battle.partition(/( против | vs )/).first.include?(' & ')
 end
 
-# :reek:TooManyStatements
-# :reek:FeatureEnvy
 def collect_names(battles)
   names_array = []
   info = {}
@@ -57,61 +65,87 @@ def collect_names(battles)
     names_array << rapper.split(' & ').first.strip
     names_array << rapper.split(' & ')[1].strip if double?(battle)
   end
-  names_array.each { |name| info[name] = [0, 0, 0.0, 0, 0.0, {}] }
+  names_array.each { |name| info[name] = [0, 0, 0.0, 0, 0.0, {}, 0] }
   info
 end
 
-# :reek:TooManyStatements
-# :reek:NestedIterators
-# :reek:FeatureEnvy
-def analyze(all_battles, rapper, info)
+def collect_rapper_battles(all_battles, rapper)
   rapper_battles = []
   all_battles.each do |battle|
     rapper_battles << battle if battle.partition(/( против | vs | VS )/).first.include?(rapper)
   end
-  info[rapper][0] = rapper_battles.size
-  all_words = 0
-  rapper_battles.each do |battle|
-    counter = 0
-    IO.read(battle).each_line { |line| counter += 1 if line[/Раунд \d/] }
-    counter = 1 if counter.zero?
-    info[rapper][1] = counter
-    all_words = lyrics_from(battle, rapper).split.size
-    lyrics_from(battle, rapper).split.each do |word|
-      info[rapper][3] += 1 if word.include?('*') || RussianObscenity.obscene?(word)
-    end
-  end
-  info[rapper][2] = (all_words.to_f / info[rapper][1]).round(2)
-  info[rapper][4] = (info[rapper][3].to_f / info[rapper][0]).round(2)
-  info = info.sort.to_h
+  rapper_battles
 end
 
-# :reek:TooManyStatements
-# :reek:UtilityFunction
-def lyrics_from(battle, rapper = '')
-  text = IO.read(battle)
-  lyrics = ''
-  if rapper != ''
-    do_write = false
-    text.each_line do |line|
-      if do_write
-        if line.start_with?(/\w+:/)
-          do_write = false
-        else
-          lyrics += ' ' + line.strip
-        end
-      elsif line.include?("#{rapper}:")
-        do_write = true
-        lyrics += ' ' + line.split("#{rapper}:")[1].strip
-      end
-    end
-  else
-    text.each_line { |line| lyrics += ' ' + line.strip }
+def rounds_in(battle)
+  counter = 0
+  IO.read(battle).each_line { |line| counter += 1 if line[/Раунд \d/] }
+  counter = 1 if counter.zero?
+  counter
+end
+
+def all_words_in(battle, rapper)
+  lyrics_from(battle, rapper).split
+end
+
+def bad_words_in(battle, rapper)
+  counter = 0
+  all_words_in(battle, rapper).each { |word| counter += 1 if word.include?('*') || RussianObscenity.obscene?(word) }
+  counter
+end
+
+def fill_counted_info(rapper, rapper_battles, info)
+  info = fill_info(rapper, rapper_battles, info)
+  info[2] = (info[6].to_f / info[1]).round(2)
+  info[4] = (info[3].to_f / info[0]).round(2)
+  info
+end
+
+def fill_info(rapper, rapper_battles, info)
+  rapper_battles.each do |battle|
+    info[1] += rounds_in(battle)
+    info[6] += all_words_in(battle, rapper).size
+    info[3] += bad_words_in(battle, rapper)
   end
-  lyrics.split(/[.,!?:;-]/).join(' ')
+  info
+end
+
+def analyze(all_battles, rapper, info)
+  rapper_battles = collect_rapper_battles(all_battles, rapper)
+  info[rapper][0] = rapper_battles.size
+  info[rapper] = fill_counted_info(rapper, rapper_battles, info[rapper])
+  info.sort.to_h
+end
+
+def lyrics_from(battle, rapper = false)
+  if double?(battle)
+    lyrics_double(battle, rapper).split(/[.,!?:;-]/).join(' ')
+  else
+    lyrics_single(battle).split(/[.,!?:;-]/).join(' ')
+  end
+end
+
+def lyrics_double(battle, rapper)
+  do_write = false
+  lyrics = ''
+  File.open(battle, 'r').each_line do |line|
+    if do_write
+      do_write = do_write?(line, rapper, do_write)
+      lyrics += ' ' + line.strip if do_write
+    end
+  end
+  lyrics.strip
+end
+
+def do_write?(line, rapper, toggle)
+  return false if line.start_with?(/\w+:/)
+  return true if line.include?("#{rapper}:") || toggle
+end
+
+def lyrics_single(battle)
+  lyrics = ''
+  File.open(battle, 'r').each_line { |line| lyrics += ' ' + line.strip }
+  lyrics.strip
 end
 
 run
-# rubocop:enable Metrics/AbcSize
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/PerceivedComplexity
