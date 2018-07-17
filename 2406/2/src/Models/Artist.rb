5041d0f4a9bc8033d46b4af9fbf4ed58e3bdcc32
Equinox <vereskovskystas@gmail.com>
2018-07-17 12:52:12 +0300

    Add formatter output

--- 2406/2/src/Models/Artist.rb
+++ 2406/2/src/Models/Artist.rb
@@ -6,18 +6,24 @@ class Artist
     @battle_list << battle
   end
 
-  def get_bad_words
+  def get_bad_words_capacity
     counter = 0
     @battle_list.each {|battle| battle.each{|word| counter += 1 if word.include?('*') || RussianObscenity.obscene?(word)}}
     counter
   end
 
   def get_words_in_battle_average
-    0.0
+    counter = 0
+    @battle_list.each {|battle| battle.each{|word| counter += 1}}
+    counter/get_battle_capacity
   end
 
   def get_words_in_round_average
-    0.0
+    get_words_in_battle_average/3
+  end
+
+  def get_battle_capacity
+    @battle_list.size
   end
 
   def initialize(name)
--- 2406/2/src/versus.rb
+++ 2406/2/src/versus.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 Dir['Models/*.rb'].each {|file| load(file) }
 Dir['DAO/*.rb'].each {|file| require_relative file }
+require 'terminal-table'
 
 BAD_WORDS_KEY = '--top-bad-words'
 ARTIST_NAME_KEY = '--name'
@@ -10,14 +11,23 @@ HELP_KEY = '--help'
 
 DEFAULT_RANGE = 30
 
-def bad_words(range)
+def bad_words(*args)
+  range = args[0] == nil ? DEFAULT_RANGE : Integer(args[0])
   if !(1..100).cover?(range)
     puts('Incorrect range taken.')
     exit(1)
   end
+  buffer = []
   base_of_artists = DAO::BattleDAO.new.get_artist_list_from_battles('../data/battle_text/*')
-  base_of_artists.each {|artist| puts "#{artist.name}) #{artist.get_bad_words}"}
-  #print base_of_artists
+  base_of_artists = base_of_artists.sort_by(&:get_battle_capacity).reverse
+  base_of_artists = base_of_artists.uniq
+  table = Terminal::Table.new do |row|
+    base_of_artists[0..range].each do |artist|
+       buffer <<  "#{artist.name} #{artist.get_battle_capacity} #{artist.get_bad_words_capacity} #{artist.get_words_in_battle_average} #{artist.get_words_in_round_average}"
+                                       row << ["#{artist.name}", "#{artist.get_battle_capacity} батлов", "#{artist.get_bad_words_capacity} нецензурных слов", "#{artist.get_words_in_battle_average} слов на баттл", "#{artist.get_words_in_round_average} слов в раунде"]
+                                     end
+  end
+  puts table
 end
 
 def top_words(*args)
@@ -57,7 +67,7 @@ def main
   ARGV.each { |arg| include_in_valid_keys = valid_keys.any?{ |valid_key| arg.include?(valid_key) }}
   puts('No valid keys taken. Using key "--help" to get help.') if !include_in_valid_keys || !(1..2).include?(ARGV.size)
   get_help() if ARGV.size == 1 && ARGV.include?(HELP_KEY)
-  bad_words(Integer(String.new((ARGV[0]).slice(BAD_WORDS_KEY.length + 1, ARGV[0].length)))) if ARGV.size == 1 && ARGV[0].include?(BAD_WORDS_KEY)
+  bad_words(String.new(ARGV[0]).slice(BAD_WORDS_KEY.length + 1, ARGV[0].length)) if ARGV.size == 1 && ARGV[0].include?(BAD_WORDS_KEY)
   top_words(String.new(ARGV[0]).slice(ARTIST_NAME_KEY.length + 1, ARGV[0].length)) if ARGV.size == 1 && ARGV[0].include?(ARTIST_NAME_KEY)
   top_words(String.new(ARGV[0]).slice(RANGE_KEY.length + 1, ARGV[0].length), String.new(ARGV[1]).slice(ARTIST_NAME_KEY.length + 1, ARGV[1].length)) if ARGV.size == 2 && ARGV[1].include?(ARTIST_NAME_KEY) && ARGV[0].include?(RANGE_KEY)
   plagiat() if ARGV.size == 1 && ARGV.include?(PLAG_KEY)
