51226f84615a5ff7e1b506f7010ead44cc9c1990
SadTigger <rammiel@yandex.ru>
2018-07-17 22:39:37 +0300

    Rubocop check & fixes. Add Gemfile

--- 2217/2/versus.rb
+++ 2217/2/versus.rb
@@ -1,9 +1,3 @@
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/LineLength
-# rubocop:disable Metrics/PerceivedComplexity
-
 require 'rake'
 require 'russian_obscenity'
 require 'optparse'
@@ -39,55 +33,53 @@ RAPPERS_NAMES = {
 
 @names = Array.new(0)
 
-# This method smells of :reek:DuplicateMethodCall and :reek:TooManyStatements
-# This method smells of :reek:NestedIterators and :reek:UtilityFunction
-def exclude_repeated_names(names)
-  tmp = []
-  names.each do |first_name|
-    names.each do |second_name|
-      if first_name.size != second_name.size && (first_name.include?(second_name) || second_name.include?(first_name))
-        tmp = first_name.size > second_name.size ? first_name : second_name
-        names.delete(tmp) if tmp
-      end
+def rapper_list_check
+  array = collect_all_names
+  RAPPERS_NAMES.each_key do |key|
+    array.each_with_index do |item, _item_index|
+      array[array.index(key)] = RAPPERS_NAMES[key] if item == key
     end
   end
-  names.delete(tmp) if tmp
-  names.sort.uniq!
-  RAPPERS_NAMES.each_key do |key|
-    next unless names.any?(key)
-    names[names.index(key)] = RAPPERS_NAMES[key]
-    names[names.index(key)] = RAPPERS_NAMES[key] if names.index(key)
-  end
-  names.sort.uniq!
+  array
 end
 
-# This method smells of :reek:FeatureEnvy
-def get_all_names(filename)
-  return filename[0, filename.index('&')].strip! if filename.include?('&')
+def exclude_repeated_names(array)
+  tmp = []
+  [array, array].each do |first_name, second_name|
+    if first_name.size != second_name.size && (first_name.include?(second_name) || second_name.include?(first_name))
+      tmp = first_name.size > second_name.size ? first_name : second_name
+    end
+  end
+  array.delete(tmp) if tmp
+  array
+end
 
+def file_name_check(filename)
   if filename.include?('против')
-    filename[0, filename.index('против')].strip!
-  elsif filename.include?('vs')
-    filename[0, filename.index('vs')].strip!
+    filename.include?('aka') ? filename[0, filename.index('aka')].strip! : filename[0, filename.index('против')].strip!
   elsif filename.include?('VS')
     filename[0, filename.index('VS')].strip!
-  else
-    puts 'Что-то пошло не так.'
+  elsif filename.include?('vs')
+    filename[0, filename.index('vs')].strip!
   end
 end
 
-# This method smells of :reek:TooManyStatements and :reek:UtilityFunction
-# This method smells of :reek:NestedIterators
+def collect_all_names
+  all_names = []
+  filelist = FileList.new('*против*', '*vs*', '*VS*').exclude('*.rb')
+  filelist.each { |filename| all_names << file_name_check(filename) }
+  all_names.sort.uniq!
+end
+
 def count_bad_words(name)
   counts = []
-  filelist = FileList.new('*против*', '*vs*', '*VS*')
-  filelist.exclude('*.rb')
+  filelist = FileList.new('*против*', '*vs*', '*VS*').exclude('*.rb')
   filelist.each do |filename|
     next unless filename.include?(name)
-    file = File.open(filename)
-    words = file.read
-    bad_words = words.downcase.tr(',.?&quot;!', '').split(' ').select { |word| word.match(/[а-яА-Я]+[*][а-я]+/) || RussianObscenity.obscene?(word) }
-    counts << bad_words
+    words = File.read(filename)
+    counts << words.downcase.tr(',.?&quot;!', '')
+                   .split(' ')
+                   .select { |word| word.match(/[а-яА-Я]+[*][а-я]+/) || RussianObscenity.obscene?(word) }
   end
   counts.flatten.count
 end
@@ -96,7 +88,6 @@ def average_bad_words(name)
   (count_bad_words(name) / find_battles(name).to_f).round(2)
 end
 
-# This method smells of :reek:TooManyStatements and :reek:UtilityFunction
 def find_average_round_words(name)
   battle_texts = []
   words_count = 0
@@ -106,17 +97,12 @@ def find_average_round_words(name)
       battle_texts << text
     end
   end
-  battle_texts.each do |battle|
-    words_count += battle.split.count
-  end
+  battle_texts.each { |battle| words_count += battle.split.count }
   words_count / (battle_texts.count * 3)
 end
 
-# This method smells of :reek]:TooManyStatements and :reek:UtilityFunction
-# This method smells of :reek:TooManyStatements
 def find_battles(name)
   all_battles = []
-
   Dir.glob('*[^.rb]') do |filename|
     battle = filename.match(name)
     all_battles << battle
@@ -125,12 +111,7 @@ def find_battles(name)
   all_battles.count
 end
 
-all_names = []
-
-filelist = FileList.new('*против*', '*vs*', '*VS*')
-filelist.exclude('*.rb')
-filelist.each { |filename| all_names << get_all_names(filename) }
-@names = exclude_repeated_names(all_names)
+@names = exclude_repeated_names(rapper_list_check)
 
 def battle_hash
   hash = @names.each_with_object(Hash.new(0)) do |rapper, total_bad_words|
@@ -145,15 +126,13 @@ OptionParser.new do |options|
     table = Terminal::Table.new do |terminal|
       rapper_name = battle_hash.reverse[0...number.to_i].to_h
       rapper_name.each do |rapper, bad_words|
-        terminal << [rapper, "#{find_battles(rapper)} батлов", "#{bad_words} нецензурных слов",
-                     "#{average_bad_words(rapper)} слов на баттл", "#{find_average_round_words(rapper)} слов в раунде"]
+        terminal << [rapper,
+                     "#{find_battles(rapper)} батлов",
+                     "#{bad_words} нецензурных слов",
+                     "#{average_bad_words(rapper)} слов на баттл",
+                     "#{find_average_round_words(rapper)} слов в раунде"]
       end
     end
     puts table
   end
 end.parse!
-# rubocop:enable Metrics/AbcSize
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/LineLength
-# rubocop:enable Metrics/PerceivedComplexity
