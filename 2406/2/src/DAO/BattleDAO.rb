ddb73eda49f047b458c65ad19f703dcf3b053551
Equinox <vereskovskystas@gmail.com>
2018-07-17 08:55:57 +0300

    Add data parsing with filters

--- 2406/2/src/DAO/BattleDAO.rb
+++ 2406/2/src/DAO/BattleDAO.rb
@@ -2,32 +2,52 @@ module DAO
 
   class BattleDAO
     def initialize
-      @alias = HelperDAO.new.read_from_file('../data/Alias.config')
+      indata_from_file = HelperDAO.new.read_from_file('../data/Alias.config')
+      @alias = []
+      indata_from_file.each_with_index do |ally, i|
+        @alias[i] = ally.gsub(/ {2,}/, '')
+        @alias[i] = @alias[i].gsub(/\n/, '')
+      end
     end
 
-    def get_battles_list(path_to_folder)
-      result = []
-      round_list = []
+    def get_artist_list_from_battles(path_to_folder)
       artists = []
-      @alias.each {|alias_name| artists << Artist.new(alias_name.split(';')[0])}
       Dir[path_to_folder].each do |file|
         buffer = HelperDAO.new.read_from_file(file)
         file = file.slice('../data/battle_text/'.length + 1, file.length)
         mc = file.split('против')[0]
+        artist_num = find_artist(artists, mc)
+        if artist_num == 0
+          artist = Artist.new(mc)
+        else
+          artist = artists[artist_num]
+        end
         parsed_text = ''
         buffer.each {|line| parsed_text += line if !line.include?('раунд') || !line.include?('Раунд')}
-        #Филтровать раунд исполнителя от ненужных слов
-        artists[find_artist_from_list(mc, artists)].add_battle(Battle.new(artists[find_artist_from_list(mc, artists)], parsed_text))
+        artist.add_battle(exclude_words(parsed_text))
+        artists << artist
       end
+      artists
     end
 
-    def find_artist_from_list(mc, artists)
-      index = 0
-      artists.each do |artist|
-        @alias.each_with_index {|name_list, i| index = i if name_list.include?(mc)}
-        artist.name
+    def find_artist(artist_list, name)
+      artist_result_num = 0
+      standard_name = name
+      @alias.each do |ally_name|
+        if ally_name.split(';').include?(name)
+          standard_name = ally_name[0]
+          break
+        end
       end
-      index
+      artist_list.each_with_index{|artist, i| artist_result_num = i if artist.name == standard_name}
+      artist_result_num
+    end
+
+    def exclude_words(indata)
+      excluded_words = HelperDAO.new.read_from_file('../template/ExcludedWords.txt')
+      indata = indata.downcase.gsub(/[.,!?:;«»<>&'()]/, '')
+      indata = indata.split(' ').select { |each_word| each_word.length >= 4}
+      indata
     end
   end
 end
--- 2406/2/src/versus.rb
+++ 2406/2/src/versus.rb
@@ -10,26 +10,30 @@ HELP_KEY = '--help'
 
 DEFAULT_RANGE = 30
 
-def bad_words()
-  puts ('Under construction')
-  #TODO: First task with bad words
+def bad_words(range)
+  if !(1..100).cover?(range)
+    puts('Incorrect range taken.')
+    exit(1)
+  end
+  base_of_artists = DAO::BattleDAO.new.get_artist_list_from_battles('../data/battle_text/*')
+  #print base_of_artists
 end
 
 def top_words(*args)
   puts ('Under construction')
-  artist = args[1]
-  range = args[0]
-  artist_exist = false
-  DAO.Artist_DAO.get_artists().each {|artist_base|
-    if artist_base.name == artist
-      artist_exist = true;
-      break;
-     end
-   }
-  if (artist == '') || (artist_exist)
-    puts('No artist with that name in base.')
-    exit(1)
-  end
+  #artist = args[1]
+  #range = args[0]
+  #artist_exist = false
+  #DAO::Artist_DAO.get_artists().each {|artist_base|
+  #  if artist_base.name == artist
+  #    artist_exist = true;
+  #    break;
+  #   end
+  # }
+  #if (artist == '') || (artist_exist)
+  #  puts('No artist with that name in base.')
+  #  exit(1)
+  #end
   if !(1..100).cover?(range)
     puts('Incorrect range taken.')
     exit(1)
@@ -51,9 +55,8 @@ def main
   include_in_valid_keys = false
   ARGV.each { |arg| include_in_valid_keys = valid_keys.any?{ |valid_key| arg.include?(valid_key) }}
   puts('No valid keys taken. Using key "--help" to get help.') if !include_in_valid_keys || !(1..2).include?(ARGV.size)
-  $base_of_battles = DAO::BattleDAO.new.get_battles_list('../data/battle_text/*')
   get_help() if ARGV.size == 1 && ARGV.include?(HELP_KEY)
-  bad_words() if ARGV.size == 1 && ARGV.include?(BAD_WORDS_KEY)
+  bad_words(Integer(String.new((ARGV[0]).slice(BAD_WORDS_KEY.length + 1, ARGV[0].length)))) if ARGV.size == 1 && ARGV[0].include?(BAD_WORDS_KEY)
   top_words(String.new(ARGV[0]).slice(ARTIST_NAME_KEY.length + 1, ARGV[0].length)) if ARGV.size == 1 && ARGV[0].include?(ARTIST_NAME_KEY)
   top_words(String.new(ARGV[0]).slice(RANGE_KEY.length + 1, ARGV[0].length), String.new(ARGV[1]).slice(ARTIST_NAME_KEY.length + 1, ARGV[1].length)) if ARGV.size == 2 && ARGV[1].include?(ARTIST_NAME_KEY) && ARGV[0].include?(RANGE_KEY)
   plagiat() if ARGV.size == 1 && ARGV.include?(PLAG_KEY)
