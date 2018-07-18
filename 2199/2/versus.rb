253a0d6debda0aa24bbf0b29167eaeffbf1b60a9
Lika Lex <lika.lex555@gmail.com>
2018-07-18 08:34:11 +0300

    Scattered functionality of Participant to new classes Battle and ParticipantStore

--- 2199/2/versus.rb
+++ 2199/2/versus.rb
@@ -2,79 +2,133 @@ require 'yaml'
 require 'bundler'
 Bundler.require
 
-ALIASES = YAML.load_file('alias.yml')
-rappers = {}
-# :reek:TooManyInstanceVariables
-# Participant container
-class Participant
-  attr_reader :name, :bad_words, :battle_count
-  def initialize(name)
-    @name = name
-    @battle_count = 0
-    @bad_words = 0
-    @words_count = 0
-    @rounds_count = 0
+# :reek:InstanceVariableAssumption
+# class Battle to collect information
+class Battle
+  def initialize(filename)
+    @filename = filename
   end
 
-  # :reek:NilCheck
-  def self.name_from_file(filename)
-    name = filename.split(/ против | vs | VS /).first.split('/').last.strip
-    ALIASES.find do |_, aliases|
-      aliases.include?(name)
-    end&.first || name
-  end
-
-  def filthy(filename)
-    @battle_count += 1
-    text = File.read(filename)
-    words = text.scan(/[а-яА-ЯёЁ*]+/)
-    words.delete('***')
-    words_and_round_counts(words, text)
-  end
-
-  def words_and_round_counts(words, text)
-    @words_count += words.length
-    rounds_count = text.scan(/Раунд \d/).length
-    @rounds_count += rounds_count.zero? ? 1 : rounds_count
-    bad_words_find(words)
-  end
-
-  def bad_words_find(words)
-    @bad_words += words.count do |word|
+  def bad_words_count
+    words.count do |word|
       word.include?('*') || RussianObscenity.obscene?(word)
     end
+  end
+
+  def words_count
+    words.count
+  end
+
+  def rounds_count
+    rounds_count = text.scan(/Раунд \d/).length
+    rounds_count.zero? ? 1 : rounds_count
+  end
+
+  private
+
+  def text
+    @text ||= File.read(@filename)
+  end
+
+  def words
+    return @words if @words
+    @words = text.scan(/[а-яА-ЯёЁ*]+/)
+    @words.delete('***')
+    @words
+  end
+end
+
+# Participant container
+class Participant
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @battles = []
+  end
+
+  def add_battle(filename)
+    @battles << Battle.new(filename)
   end
 
   def table_row
     [
       @name,
-      "#{battle_count} " + Russian.pluralize(battle_count.to_i, 'баттл', 'баттла', 'баттлов'),
-      "#{bad_words} нецензурных " + Russian.pluralize(bad_words.to_i, 'слово', 'слова', 'слов'),
+      "#{battles_count} " + Russian.pluralize(battles_count.to_i, 'баттл', 'баттла', 'баттлов'),
+      "#{bad_words_count} нецензурных " + Russian.pluralize(bad_words_count.to_i, 'слово', 'слова', 'слов'),
       "#{bad_words_per_battle.round(2)} слова на баттл ",
       "#{words_per_round.round(2)} слов в раунде"
     ]
   end
 
+  def bad_words_count
+    @bad_words_count ||= @battles.sum(&:bad_words_count)
+  end
+
   private
 
   def bad_words_per_battle
-    @bad_words.to_f / @battle_count
+    bad_words_count.to_f / battles_count
+  end
+
+  def words_count
+    @battles.sum(&:words_count)
+  end
+
+  def rounds_count
+    @battles.sum(&:rounds_count)
+  end
+
+  def battles_count
+    @battles.count
   end
 
   def words_per_round
-    @words_per_round = @words_count / @rounds_count.to_f
+    words_count / rounds_count.to_f
+  end
+end
+
+# class Participant store
+class ParticipantStore
+  attr_reader :participants
+
+  def initialize
+    @participants = load_participants
+  end
+
+  def top_by_bad_words(limit)
+    participants.values.sort_by { |participant| - participant.bad_words_count }.first(limit)
+  end
+
+  private
+
+  # :reek:DuplicateMethodCall
+  # :reek:FeatureEnvy
+  def load_participants
+    Dir.glob('rap-battles/*').each_with_object({}) do |filename, participants|
+      next unless File.file?(filename)
+      original_name = find_participant_for_file(filename)
+      participants[original_name] ||= Participant.new(original_name)
+      participants[original_name].add_battle(filename)
+    end
+  end
+
+  # :reek:NilCheck
+  def find_participant_for_file(filename)
+    name = filename.split(/ против | vs /i).first.split('/').last.strip
+    aliases.find do |_, aliases|
+      aliases.include?(name)
+    end&.first || name
+  end
+
+  def aliases
+    @aliases ||= YAML.load_file('alias.yml')
   end
 end
 
 OptionParser.new do |parser|
   parser.on('--top-bad-words=') do |top_bad_words|
-    Dir.glob('rap-battles/*') do |filename|
-      next unless File.file?(filename)
-      original_name = Participant.name_from_file(filename)
-      rappers[original_name] ||= Participant.new(original_name)
-      rappers[original_name].filthy(filename)
-    end
-    top_rapers = rappers.sort_by { |_, rapper_data| - rapper_data.bad_words }.first(top_bad_words.to_i)
-    puts Terminal::Table.new(rows: top_rapers.map { |raper_row| raper_row[1].table_row })
+    top_participants = ParticipantStore.new.top_by_bad_words(top_bad_words.to_i)
+    puts Terminal::Table.new(rows: top_participants.map(&:table_row))
   end
 end.parse!
