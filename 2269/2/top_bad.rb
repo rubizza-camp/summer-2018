6068c57e4f1be5010a1e7f670b331d5475638b08
Evenstein <star78@tut.by>
2018-07-18 21:14:19 +0300

    Task #2 - fix top_bad.rb

--- 2269/2/top_bad.rb
+++ 2269/2/top_bad.rb
@@ -20,19 +20,20 @@ def print_out(rapers, top)
   delimiter = 1
   rapers.sort_by { |_k, val| [-val.bad_words] }.each do |_key, value|
     break if delimiter > top
-    bad_words = value.bad_words
-    result = value.name.ljust(25) + ' | ' +
-             value.battles.to_s.ljust(2) + ' ' +
-             RuPropisju.choose_plural(value.battles, 'баттл', 'баттла', 'баттлов').ljust(7) + ' | ' +
-             value.bad_words.to_s.ljust(4) + ' ' +
-             RuPropisju.choose_plural(bad_words, 'нецензурное', 'нецензурных', 'нецензурных') + ' ' +
-             RuPropisju.choose_plural(bad_words, 'слово', 'слова', 'слов').ljust(5) + ' | ' +
-             bad_words.fdiv(value.battles).round(2).to_s.ljust(6) + ' ' +
-             RuPropisju.choose_plural(bad_words.fdiv(value.battles).round(2), 'слово', 'слова', 'слова') +
-             ' на баттл | '.ljust(12) + value.words_round.to_s.ljust(5) + ' ' +
-             RuPropisju.choose_plural(value.words_round, 'слово', 'слова', 'слов').ljust(5) +
-             ' в раунде |'.ljust(11)
-    puts result
+    puts result_string value
     delimiter += 1
   end
+end
+
+def result_string(raper)
+  raper.name.ljust(25) + ' | ' + raper.battles.to_s.ljust(2) + ' ' +
+    RuPropisju.choose_plural(raper.battles, 'баттл', 'баттла', 'баттлов').ljust(7) + ' | ' +
+    raper.bad_words.to_s.ljust(4) + ' ' +
+    RuPropisju.choose_plural(raper.bad_words, 'нецензурное', 'нецензурных', 'нецензурных') + ' ' +
+    RuPropisju.choose_plural(raper.bad_words, 'слово', 'слова', 'слов').ljust(5) + ' | ' +
+    raper.bad_words.fdiv(raper.battles).round(2).to_s.ljust(6) + ' ' +
+    RuPropisju.choose_plural(raper.bad_words.fdiv(raper.battles).round(2), 'слово', 'слова', 'слова') + 
+    ' на баттл | '.ljust(12) + raper.words_round.to_s.ljust(5) + ' ' +
+    RuPropisju.choose_plural(raper.words_round, 'слово', 'слова', 'слов').ljust(5) +
+    ' в раунде |'.ljust(11)
 end
