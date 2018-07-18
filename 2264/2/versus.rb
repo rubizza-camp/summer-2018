3572c63075925633eb3eee34f0c12b6997bc987b
mersXX <mers69lol@gmail.com>
2018-07-18 18:34:40 +0300

    Refactor code and add help function

--- 2264/2/versus.rb
+++ 2264/2/versus.rb
@@ -3,28 +3,17 @@ require 'terminal-table'
 # :reek:UtilityFunction
 def find_all_rappers
   rappers = []
-
-  Dir.glob('rap/*') do |file_name|
-    rapper = file_name.split('/').last.split(/против|VS\b|vs\b/)[0].strip
-    rappers << rapper
-  end
+  Dir.glob('rap/*') { |file_name| rappers << file_name.split('/').last.split(/против|VS\b|vs\b/)[0].strip }
   rappers.uniq
 end
 # :reek:UtilityFunction
-# :reek:TooManyStatements
 
 def find_battles(name)
   all_battles = []
-
-  Dir.glob('rap/*') do |file_name|
-    battle = file_name.match(name)
-    all_battles << battle
-    all_battles.reject!(&:nil?)
-  end
-  all_battles.count
+  Dir.glob('rap/*') { |file_name| all_battles << file_name.match(name) }
+  all_battles.reject!(&:nil?).count
 end
 # :reek:UtilityFunction
-# :reek:TooManyStatements
 
 def find_bad_words(name)
   all_bad_words = []
@@ -32,8 +21,7 @@ def find_bad_words(name)
   Dir.glob('rap/*') do |file_name|
     if file_name.match(name)
       text = File.read(file_name)
-      bad_word = text.scan(/уй|сос|залуп/)
-      all_bad_words << bad_word
+      all_bad_words << text.scan(/уй|сос|залуп/)
     end
   end
   all_bad_words.flatten.count
@@ -41,21 +29,17 @@ end
 
 def average_bad_words(name)
   avg = find_bad_words(name) / find_battles(name).to_f
-  avg
+  avg.round(2)
 end
 # :reek:UtilityFunction
 # :reek:TooManyStatements
-# rubocop:disable Metrics/MethodLength
 
 def find_average_round_words(name)
   battle_texts = []
   words_count = 0
 
   Dir.glob('rap/*') do |file_name|
-    if file_name.match(name)
-      text = File.read(file_name)
-      battle_texts << text
-    end
+    battle_texts << File.read(file_name) if file_name.match(name)
   end
   battle_texts.each do |battle|
     words_count += battle.split.count
@@ -63,7 +47,6 @@ def find_average_round_words(name)
   avg = words_count / (battle_texts.count * 3)
   avg
 end
-# rubocop:enable Metrics/MethodLength
 
 def battle_hash
   hash = find_all_rappers.each_with_object(Hash.new(0)) do |raper, total_bad_words|
@@ -83,5 +66,9 @@ OptionParser.new do |options|
       end
     end
     puts table
+  end
+  options.on('--help') do
+    puts 'Usage:'
+    puts 'Type --top-bad-words=N , there N - number of top rappers with bad fantasy'
   end
 end.parse!
