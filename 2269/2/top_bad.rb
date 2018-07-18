a18c4554472479871968690801359836aab1868d
Evenstein <star78@tut.by>
2018-07-18 23:12:04 +0300

    Task #2 - top_bad.rb fixed completely

--- 2269/2/top_bad.rb
+++ 2269/2/top_bad.rb
@@ -1,6 +1,26 @@
 # This method returns top Rapers uses bad words
-# :reek:TooManyStatements
-# :reek:NestedIterators
+class TopBad
+  def self.plural_battle(value)
+    value.to_s.ljust(2) + ' ' +
+      RuPropisju.choose_plural(value, 'баттл', 'баттла', 'баттлов').ljust(7) + ' | '
+  end
+
+  def self.plural_bad(value)
+    value.to_s.ljust(4) + ' ' +
+      RuPropisju.choose_plural(value, 'нецензурное слово', 'нецензурных слова', 'нецензурных слов').ljust(17) + ' | '
+  end
+
+  def self.plural_word_dec(value, end_phrase)
+    value.to_s.ljust(6) + ' ' +
+      RuPropisju.choose_plural(value, 'слово', 'слова', 'слова').ljust(5) + end_phrase
+  end
+
+  def self.plural_word(value, end_phrase)
+    value.to_s.ljust(5) + ' ' +
+      RuPropisju.choose_plural(value, 'слово', 'слова', 'слов').ljust(5) + end_phrase
+  end
+end
+
 def top_bad_word_rapers(top, destination)
   rapers = get_rapers_list destination
 
@@ -13,27 +33,18 @@ def top_bad_word_rapers(top, destination)
   print_out rapers, top
 end
 
-# :reek:DuplicateMethodCall
-# :reek:FeatureEnvy
-# :reek:TooManyStatements
 def print_out(rapers, top)
   delimiter = 1
   rapers.sort_by { |_k, val| [-val.bad_words] }.each do |_key, value|
     break if delimiter > top
-    puts result_string value
+    puts result_string value, value.bad_words.fdiv(value.battles).round(2)
     delimiter += 1
   end
 end
 
-def result_string(raper)
-  raper.name.ljust(25) + ' | ' + raper.battles.to_s.ljust(2) + ' ' +
-    RuPropisju.choose_plural(raper.battles, 'баттл', 'баттла', 'баттлов').ljust(7) + ' | ' +
-    raper.bad_words.to_s.ljust(4) + ' ' +
-    RuPropisju.choose_plural(raper.bad_words, 'нецензурное', 'нецензурных', 'нецензурных') + ' ' +
-    RuPropisju.choose_plural(raper.bad_words, 'слово', 'слова', 'слов').ljust(5) + ' | ' +
-    raper.bad_words.fdiv(raper.battles).round(2).to_s.ljust(6) + ' ' +
-    RuPropisju.choose_plural(raper.bad_words.fdiv(raper.battles).round(2), 'слово', 'слова', 'слова') + 
-    ' на баттл | '.ljust(12) + raper.words_round.to_s.ljust(5) + ' ' +
-    RuPropisju.choose_plural(raper.words_round, 'слово', 'слова', 'слов').ljust(5) +
-    ' в раунде |'.ljust(11)
+def result_string(raper, average)
+  raper.name.ljust(25) + ' | ' + TopBad.plural_battle(raper.battles) +
+    TopBad.plural_bad(raper.bad_words) +
+    TopBad.plural_word_dec(average, ' на баттл | ') +
+    TopBad.plural_word(raper.words_round, ' в раунде |')
 end
