1ae0b5e9251a6c4fac21352a8e3fd18588eea42e
mersXX <mers69lol@gmail.com>
2018-07-23 15:08:32 +0300

    Change RappersList and TopBadWordsPrinter classes, fix small inconsistencies

--- 2264/2/rappers_list.rb
+++ 2264/2/rappers_list.rb
@@ -1,8 +1,9 @@
 # This module srore all rappers
 module RappersList
   def self.find_all_rappers
-    rappers = []
-    Dir.glob('rap/*') { |file_name| rappers << file_name.split('/').last.split(/против|VS\b|vs\b/)[0].strip }
-    rappers.uniq
+    Storage.all_data.inject([]) do |array, (file_name, _file_content)|
+      array << file_name.split('/').last.split(/против|VS\b|vs\b/)[0].strip
+      array.uniq
+    end
   end
 end
