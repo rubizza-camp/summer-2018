475f736129c0f301d6bcec61509d10b42a789f59
SvintBel <svintbel@gmail.com>
2018-07-23 22:34:46 +0300

    Add new logic to battle_countes It was horrible indian code & logic was wrong, also add Gemfile &Gemfile.lock

--- 2207/2/BattleCounters.rb
+++ 2207/2/BattleCounters.rb
@@ -4,19 +4,31 @@ module Counters
   PATH_FOLDER = 'texts'.freeze
   # This method smells of :reek:UtilityFunction
   # I think it will be better to paste this code here in couse of small project
+  def read_files_with_buttles(battle)
+      Dir.chdir(PATH_FOLDER) { File.read(battle) }
+  end
+
   def count_normal(battles)
     words = 0
+    lines = []
+    lines_with_value = []
     battles.each do |battle|
-      words += Dir.chdir(PATH_FOLDER) { File.read(battle) }
+      words += read_files_with_buttles(battle)
                   .gsub(/[.!-?,:]/, ' ').strip.split.count
     end
-    words / (battles.size * 3)
-  end
+    battles.each do |battle|
+    lines = Dir.chdir(PATH_FOLDER) { File.readlines(battle) }
+    end
+    revelant_lines = lines.find_all {|line| line.include?("Раунд") }
+    revelant_lines = [1] if revelant_lines.count == 0
+    words / revelant_lines.count
+    end
+
 
   def count_bad(battles)
     bad_words = 0
     battles.each do |battle|
-      bad_words += count_bad_words(Dir.chdir(PATH_FOLDER) { File.read(battle) })
+      bad_words += count_bad_words(read_files_with_buttles battle)
     end
     bad_words
   end
@@ -24,11 +36,7 @@ module Counters
   # I think it will be better to paste this code here in couse of small project
 
   def count_bad_words(file)
-    bad_words = 0
-    file.split.each do |word|
-      bad_words += 1 if RussianObscenity.obscene?(word)
-    end
-    bad_words += file.count('*')
-    bad_words
+  file.split.each_with_object([]) { |word, arr| arr << word if RussianObscenity.obscene?(word) }.count
   end
+
 end
---
+++ 2207/2/ex.rb
@@ -0,0 +1,26 @@
+require 'russian_obscenity'
+PATH_FOLDER = 'texts'.freeze
+
+  def find_rapers_titles(raper)
+    rapers_titles = []
+    Dir.chdir(PATH_FOLDER) do
+      Dir.glob("*#{raper}*").each do |title|
+        rapers_titles << title if title.split('против').first.include? raper
+      end
+    end
+    rapers_titles
+  end
+
+def read_files_with_buttles(battle)
+      Dir.chdir(PATH_FOLDER) { File.read(battle) }
+ end
+  def count_bad_words(file)
+#    bad_words = []
+#    file.split.each do |word|
+#      bad_words << word if RussianObscenity.obscene?(word)
+#    end
+#    bad_words.count
+  file.split.each_with_object([]) { |word1, arr| arr << word1 if RussianObscenity.obscene?(word1) }.count
+  end
+a = read_files_with_buttles('1')
+p count_bad_words a
