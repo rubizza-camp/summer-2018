ad0ea04db7b09bce4fc4da7af453050177309ed8
Nikita <nikitakholdobin@yandex.ru>
2018-07-23 14:21:19 +0300

    Fixes

--- 2346/2/rhymes_pairs_methods.rb
+++ 2346/2/rhymes_pairs_methods.rb
@@ -1,9 +1,14 @@
 module Rap
   class RhymesPairsMethods
-    def self.all_rhymes_pairs_excluding_one(rhymes_objects, one_object)
+    attr_reader :excluded_object
+    def initialize(excluded_object)
+      @excluded_object = excluded_object
+    end
+
+    def all_rhymes_pairs_without_excluded(rhymes_objects)
       rhymes_pairs_list = []
       rhymes_objects.each do |rhyme_object|
-        rhymes_pairs_list << rhyme_object.rhymes if rhyme_object != one_object
+        rhymes_pairs_list << rhyme_object.rhymes if rhyme_object != excluded_object
       end
       rhymes_pairs_list.flatten
     end
