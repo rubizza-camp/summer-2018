6ececdc288a8ae9078a26685c58123b537fce28f
lstpsche <lstpsche@gmail.com>
2018-07-24 16:02:18 +0300

    Fixed method with bang and one enumerable.

--- 2312/2/battlers_table.rb
+++ 2312/2/battlers_table.rb
@@ -6,15 +6,12 @@ class BattlersTable
   attr_reader :num_top, :battlers
   def initialize(battlers, num_top)
     @battlers = battlers
-    @table = []
     @num_top = num_top
   end
 
   def print
     terminate_with('ERROR: top number is zero') if num_top.zero?
-
-    create_table!
-    puts Terminal::Table.new rows: @table[0...num_top]
+    puts Terminal::Table.new rows: create_table[0...num_top]
   end
 
   private
@@ -25,16 +22,8 @@ class BattlersTable
   end
 
   def create_table
-    table = []
-    @battlers.sort_by { |_name, info| info.stats.rapper_stats[:bad_words_num] }.reverse.to_h.keys.each do |name|
-      table << TableFiller.new(name, battlers[name]).filled_line
-    end
-    table
-  end
-
-  def create_table!
-    @battlers.sort_by { |_name, info| info.stats.rapper_stats[:bad_words_num] }.reverse.to_h.keys.each do |name|
-      @table << TableFiller.new(name, battlers[name]).filled_line
+    @battlers.sort_by { |_name, info| info.stats.rapper_stats[:bad_words_num] }.reverse.to_h.keys.map do |name|
+      TableFiller.new(name, battlers[name]).filled_line
     end
   end
 end
