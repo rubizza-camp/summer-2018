e1808b5878a286a6efacb91891bc783abe04f356
shamritskiy3468 <shamritskiy3468@gmail.com>
2018-07-24 13:01:50 +0300

    Fixed Hound error about service class and other small mistakes

--- 2197/2/service.rb
+++ 2197/2/service.rb
@@ -2,6 +2,10 @@ require_relative 'artist'
 require_relative 'constants'
 # Service class
 class Service
+  def initialize
+    @state = :parsing
+  end
+
   def battlers
     Dir.glob(PATH_TO_FILES).each_with_object({}) do |file_name, rappers|
       next unless File.file?(file_name)
@@ -12,5 +16,6 @@ class Service
   def battlers_parser(file_name, rappers)
     rapper_name = file_name.split(REGEX_FOR_NAMES).first.split('/').last.strip
     (rappers[rapper_name] ||= Artist.new(rapper_name)).add_battles(file_name)
+    @state = :parsed
   end
 end
