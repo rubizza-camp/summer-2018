8f9a77d0f7ae240dc7537e3e106c48056d8f509d
Vladislav <ilyuschitsvlad@gmail.com>
2018-07-25 13:16:15 +0300

    Update RapperAnalyzer.rb

--- 2227/2/lib/RapperAnalyzer.rb
+++ 2227/2/lib/RapperAnalyzer.rb
@@ -21,9 +21,17 @@ class RapperAnalyzer
     @bad_words_counter ||= count_bad_words
   end
 
+  def avg_bad_words_in_battle
+    bad_words_counter / number_of_battles.to_f
+  end
+
   def rounds_of_rappers
     rounds = battles_text.scan(/Раунд\s\d/).size
     rounds.zero? ? DEFAULT_ROUNDS_COUNT : rounds
+  end
+
+  def count_words_in_rounds
+    battles_words.size.to_f / rounds_of_rappers
   end
 
   def battles_words
