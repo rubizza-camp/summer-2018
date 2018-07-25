c52b9a5927935c48472aab5914fb45e54bdd8873
SergeiHrenovsky <sergeihrenovsky@yandex.by>
2018-07-25 22:09:46 +0300

    added counting words to the rapper

--- 2309/2/raper.rb
+++ 2309/2/raper.rb
@@ -3,7 +3,7 @@ require_relative 'battle.rb'
 # Class one raper
 class Raper
 
-  attr_reader :battles
+  attr_reader :battles, :all_words, :bad_words
 
   def initialize(namee)
     @name = namee
@@ -22,4 +22,29 @@ class Raper
       file.index(@name) == 0 ? @battles.push(Battle.new(file)) : next
     end
   end
+
+  def count_all_words
+    @battles.each do |battle|
+      battle.count_all_words
+      @all_words += battle.sum_all_words
+    end
+  end
+
+  def count_bad_words
+    @battles.each do |battle|
+      battle.count_bad_words
+      @bad_words += battle.sum_bad_words
+    end
+  end
+
 end
+
+a = Raper.new('Oxxxymiron')
+a.count_battles
+a.count_all_words
+a.count_bad_words
+
+puts a.battles.inspect
+
+#puts a.all_words.inspect
+#puts a.bad_words.inspect
