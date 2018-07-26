ab632ec72e614cab56591e3db48fb58027743abb
dixent <dixent@inbox.ru>
2018-07-26 16:17:18 +0300

    Change requests. Fixed cosmetic things

--- 2363/3/config.ru
+++ 2363/3/config.ru
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-Bundler.require(:default, :development)
+Bundler.require(:development)
 Dir.glob('./{controllers,helpers,models}/*.rb').sort.each { |file| require file }
 Post.redis = Redic.new('redis://127.0.0.1:6379/0')
-Comment.redis = Redic.new('redis://127.0.0.1:6379/1')
+Comment.redis = Redic.new('redis://127.0.0.1:6379/0')
 
 use Rack::MethodOverride
 map('/') { run PostsController }
--- 2363/3/controllers/posts_controller.rb
+++ 2363/3/controllers/posts_controller.rb
@@ -3,16 +3,20 @@
 # Class posts controller
 class PostsController < ApplicationController
   get '/' do
+    redirect '/posts'
+  end
+
+  get '/posts' do
     @posts = Post.all.sort_by(:rating).reverse!
     slim :'post/show'
   end
 
-  get '/add' do
-    slim :'post/add'
+  get '/posts/new' do
+    slim :'post/new'
   end
 
-  post '/add' do
-    redirect '/' unless params[:link].include?('tech.onliner.by')
+  post '/posts' do
+    redirect '/posts' unless params[:link].include?('tech.onliner.by')
     Post.all.each do |post|
       post.delete if post.link == params[:link]
     end
@@ -22,23 +26,23 @@ class PostsController < ApplicationController
     comments.zip(rating).each do |obj|
       post.comments.add(Comment.create(text: obj.first, rating: obj.last))
     end
-    redirect '/'
+    redirect '/posts'
   end
 
-  get '/post/:id' do
+  get '/posts/:id' do
     @posts = Post.all
     @post = @posts[params[:id]]
     slim :'post/index'
   end
 
-  delete '/post/delete/:id' do
+  delete '/posts/:id' do
     @post = Post.all[params[:id]]
     @post.delete
-    redirect '/'
+    redirect '/posts'
   end
 
-  delete '/post/delete_all' do
+  delete '/posts' do
     Post.all.each(&:delete)
-    redirect '/'
+    redirect '/posts'
   end
 end
