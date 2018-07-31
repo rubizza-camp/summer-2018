d7a640f71197f7c268f066f7df0f9588f4cefbe1
Marie <triangletrue@yahoo.com>
2018-07-31 16:02:32 +0300

    Parser fully fixed

--- 2377/3/sinatra/parser.rb
+++ 2377/3/sinatra/parser.rb
@@ -4,16 +4,20 @@ require 'net/http'
 require 'json'
 # Parsing of comments
 class Parser
- 
+  ONLINER = 'https://comments.api.onliner.by/news/tech.post/'.freeze
   def get_comments(link)
-    json_comments = JSON.parse(Net::HTTP.get_response(URI.parse('https://comments.api.onliner.by/news/tech.post/' + get_id(link) + '/comments?limit=50')).body)
+    json_comments = JSON.parse(Net::HTTP.get_response(URI.parse(ONLINER + get_id(link) + '/comments?limit=50')).body)
     comments = json_comments['comments'].each.map { |hash| hash['text'] }
-    comments.each do |comment|
-        link.comments.push(Comment.create(comment: comment))
-    end
+    push(comments, link)
   end
 
   private
+
+  def push(comments, link)
+    comments.each do |comment|
+      link.comments.push(Comment.create(comment: comment))
+    end
+  end
 
   def response(link)
     Net::HTTP.get_response(URI.parse(link.link)).body
@@ -22,6 +26,4 @@ class Parser
   def get_id(link)
     Nokogiri::HTML(response(link)).xpath('//span[@news_id]').to_s.match(/\d+/).to_s
   end
-
-
 end
