25b289f362707f968e379149192cea2ff2523ad5
Kiryl Klimovich <kirylklimovich@gmail.com>
2018-07-31 14:31:26 +0300

    task 3: sinatra project which parse onliner comments from posts urls

---
+++ 2249/3/sinatra-onliner/application_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'sinatra/config_file'
+
+# Main controller
+
+class ApplicationController < Sinatra::Base
+  set :views, File.expand_path(File.join(__FILE__, '../../views'))
+  register Sinatra::ConfigFile
+  config_file '../config.yml'
+end
